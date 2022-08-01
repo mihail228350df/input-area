4ca7d1725ac23d476c9454f24018f72be3aa0257
Михаил Сорокин <mihailsorokin@gmail.com>
2022-08-01 15:45:21 +0300

    fin

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const inputNode = document.querySelector(".js-input");
+const btnNode = document.querySelector(".js-btn");
+const outputNode = document.querySelector(".js-output");
+
+let title = '';
+
+function changeTitle(newTitle) {
+    title = newTitle;
+}
+
+function render() {
+    outputNode.innerHTML = `<h1>${title}</h1>`;
+
+    
+}
+
+btnNode.addEventListener("click", function() {
+    changeTitle(inputNode.value);
+    render();
+});
+
